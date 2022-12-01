1b7524f37821044757a85b0e9aa46ba24b854820
Louis Sanna <louis.sanna@ekimetrics.com>
2022-12-01 22:05:16 +0100

    feat(day1): solve part2

--- days/day01/solve.py
+++ days/day01/solve.py
@@ -8,6 +8,11 @@ raw_inventories = list(
 inventories = [list(map(int, raw_inventory)) for raw_inventory in raw_inventories]
 
 sums = list(map(sum, inventories))
-result = max(sums)
+result1 = max(sums)
 
-print("result 1:", result)
+print("result 1:", result1)
+
+sorted_sums = sorted(sums)
+result2 = sum(sorted_sums[-3:])
+
+print("result 2:", result2)
