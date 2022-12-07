1b43f7ad083ba725747e4ed4d3c6c4d594b80c53
Louis Sanna <louis.sanna@ekimetrics.com>
2022-12-07 20:26:27 +0100

    chore(day07): clean code

--- days/day07/solve.py
+++ days/day07/solve.py
@@ -1,24 +1,53 @@
 """
 Solves Advent of code 2022 day07
 """
+from typing import List
 
 
 class File:
-    def __init__(self, name, size: int, is_dir, parent=None):
+    """
+    This class represents a file or directory in a file system. It has a name,
+    size, a flag indicating whether it is a directory, and a list of children
+    (if it is a directory). It also has a reference to its parent directory.
+    """
+
+    def __init__(self, name: str, size: int, is_dir: bool, parent=None):
+        """
+        Initializes a new File object.
+
+        Args:
+            name (str): The name of the file.
+            size (int): The size of the file in bytes.
+            is_dir (bool): A flag indicating whether the file is a directory.
+            parent (File): The parent directory of the file.
+        """
         self.name = name
         self.size = size
         self.is_dir = is_dir
-        self.children: list[File] = []
+        self.children: List[File] = []
         self.parent: File = parent
 
     def total_size(self) -> int:
+        """
+        Returns the total size of the file, including all subdirectories and files
+        if it is a directory.
+        """
         if self.is_dir:
             return self.size + sum([c.total_size() for c in self.children])
         else:
             return self.size
 
 
-def create_tree(lines: list[str]):
+def create_tree(lines: List[str]):
+    """
+    Creates a tree of File objects from the given lines.
+
+    Args:
+        lines (List[str]): The list of lines containing the file information.
+
+    Returns:
+        File: The root of the tree.
+    """
     root_dir = File("/", 0, True)
     cur_dir = root_dir
 
@@ -49,7 +78,13 @@ def create_tree(lines: list[str]):
     return root_dir
 
 
-def solve_part1(lines: list[str]):
+def solve_part1(lines: List[str]):
+    """
+    Solves part 1 of the problem.
+
+    Args:
+        lines (List[str]): The list of lines containing the file information.
+    """
     root = create_tree(lines)
 
     result: int = 0
@@ -63,16 +98,22 @@ def solve_part1(lines: list[str]):
 
         current_trees = [child for tree in current_trees for child in tree.children]
 
-    print("Part 1 answer:", result)
+    return result
 
 
-def solve_part2(lines: list[str]):
+def solve_part2(lines: List[str]):
+    """
+    Solves part 2 of the problem.
+
+    Args:
+        lines (List[str]): The list of lines containing the file information.
+    """
     root = create_tree(lines)
 
     result: int = 0
 
-    current_trees: list[File] = [root]
-    dirs: list[File] = []
+    current_trees: List[File] = [root]
+    dirs: List[File] = []
 
     while len(current_trees) > 0:
         for tree in current_trees:
@@ -88,11 +129,20 @@ def solve_part2(lines: list[str]):
             result = directory.total_size()
             break
 
-    print("Part 2 answer:", result)
+    return result
 
 
 with open("input.txt", encoding="utf-8") as file_descriptor:
     file_lines = file_descriptor.read().strip().split("\n")
 
-solve_part1(file_lines)
-solve_part2(file_lines)
+
+def main():
+    """
+    The main entry point of the program.
+    """
+    print("Part 1 answer:", solve_part1(file_lines))
+    print("Part 2 answer:", solve_part2(file_lines))
+
+
+if __name__ == "__main__":
+    main()
