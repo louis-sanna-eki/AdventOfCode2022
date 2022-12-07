31617fc9ddc9606ed2c74c9ff1ab22db6e1802f3
Louis Sanna <louis.sanna@ekimetrics.com>
2022-12-07 18:53:11 +0100

    chore(day06): clean code

--- days/day06/solve.py
+++ days/day06/solve.py
@@ -1,29 +1,42 @@
-with open("input.txt") as input_file:
-    input = input_file.read().strip()
+"""
+Solves Advent of code 2022 day06
+"""
+
+with open("input.txt", encoding="utf-8") as file_descriptor:
+    datastream = file_descriptor.read().strip()
 
 START_PACKET_MARKER_LENGTH = 4
 START_MESSAGE_MARKER_LENGTH = 14
 
 
-def find_first_marker_index(letters, marker_length):
-    queue = []
-    result = None
+def find_first_marker_index(letters: list[str], marker_length: int) -> int:
+    """
+    Finds the first marker index in the given list of letters.
 
-    for index, letter in enumerate(letters):
-        if len(queue) < marker_length:
-            queue.append(letter)
-            continue
-        if len(set(queue)) == marker_length:
-            result = index
-            break
-        queue.pop(0)
-        queue.append(letter)
+    Args:
+        letters: A list of letters to search through.
+        marker_length: The length of the marker to search for.
 
-    return result
+    Returns:
+        The index of the first marker in the list, or -1 if no marker was found.
+    """
+    for (i, _) in enumerate(letters):
+        if len(set(letters[i : (i + marker_length)])) == marker_length:
+            return i + marker_length
+
+    return -1
 
 
-start_of_packet = find_first_marker_index(input, START_PACKET_MARKER_LENGTH)
-print("start_of_packet", start_of_packet)
+def main():
+    """
+    The main entry point of the program.
+    """
+    start_of_packet = find_first_marker_index(datastream, START_PACKET_MARKER_LENGTH)
+    print("start_of_packet", start_of_packet)
 
-start_of_message = find_first_marker_index(input, START_MESSAGE_MARKER_LENGTH)
-print("start_of_message", start_of_message)
+    start_of_message = find_first_marker_index(datastream, START_MESSAGE_MARKER_LENGTH)
+    print("start_of_message", start_of_message)
+
+
+if __name__ == "__main__":
+    main()
