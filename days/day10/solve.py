7793c15dfdc444d460abc098c30cda732329c7ba
Louis Sanna <louis.sanna@ekimetrics.com>
2022-12-10 16:17:26 +0100

    feat(day(10): solve part2

--- days/day10/solve.py
+++ days/day10/solve.py
@@ -27,6 +27,33 @@ def solve_part1(commands: List[str]):
     return result
 
 
+def solve_part2(commands: List[str]):
+    tasks: List[Callable[[int], int]] = list()
+    for command in commands:
+        name = command.split(" ")[0]
+        if name == "noop":
+            tasks.append(lambda x: x)
+        if name == "addx":
+            to_add = int(command.split(" ")[1])
+            tasks.append(lambda x: x)
+            tasks.append(make_adder(to_add))
+
+    result = ""
+    register = 1
+    for i in range(0, 240):
+        task = tasks[i]
+        position = i % 40
+        if position == 0:
+            result += "\n"
+        if register - 1 <= position <= register + 1:
+            result += "#"
+        else:
+            result += "."
+
+        register = task(register)
+    print(result)
+
+
 with open("input.txt", encoding="utf-8") as file_descriptor:
     commands = file_descriptor.read().strip().split("\n")
 
@@ -36,6 +63,7 @@ def main():
     The main entry point of the program.
     """
     print("Part 1 answer:", solve_part1(commands))
+    solve_part2(commands)
 
 
 if __name__ == "__main__":
