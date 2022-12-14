1defee3e9ffec3698b7cf67b49cca4f540d39b2b
Louis Sanna <louis.sanna@ekimetrics.com>
2022-12-14 21:05:28 +0100

    solve part2

--- days/day14/solve.py
+++ days/day14/solve.py
@@ -49,6 +49,59 @@ def solve_part1(cave: Dict[Tuple[int, int], int]):
     return grain_count
 
 
+def parse2(lines: List[str]) -> Dict[Tuple[int, int], int]:
+    result: Dict[Tuple[int, int], int] = dict()
+    max_y = 0
+    for i in range(1000):
+        for j in range(1000):
+            result[(i, j)] = "."
+    for line in lines:
+        points = [
+            point
+            for point in map(
+                lambda coord: tuple(map(int, coord.split(","))), line.split(" -> ")
+            )
+        ]
+        for index, point in enumerate(points[:-1]):
+            next_point = points[index + 1]
+            start_i = min(point[0], next_point[0])
+            stop_i = max(point[0], next_point[0]) + 1
+            for i in range(start_i, stop_i):
+                start_j = min(point[1], next_point[1])
+                stop_j = max(point[1], next_point[1]) + 1
+                for j in range(start_j, stop_j):
+                    result[(i, j)] = "#"
+                    max_y = max(max_y, j)
+    for i in range(1000):
+        j = max_y
+        result[(i, max_y + 2)] = "#"
+    return result
+
+
+# BEWARE SIDE-EFFECTS
+def solve_part2(cave: Dict[Tuple[int, int], int]):
+    sand = (500, 0)
+    grain_count = 0
+    while True:
+        (x, y) = sand
+        if cave.get((x, y + 1), "?") == ".":
+            sand = (x, y + 1)
+            continue
+        if cave.get((x - 1, y + 1), "?") == ".":
+            sand = (x - 1, y + 1)
+            continue
+        if cave.get((x + 1, y + 1), "?") == ".":
+            sand = (x + 1, y + 1)
+            continue
+        cave[sand] = "o"
+        grain_count += 1
+        if (500, 0) == sand:
+            break
+        sand = (500, 0)
+
+    return grain_count
+
+
 with open("input.txt", encoding="utf-8") as file_descriptor:
     lines = file_descriptor.read().strip().split("\n")
 
@@ -59,6 +112,8 @@ def main():
     """
     cave = parse(lines)
     print("Solve part1 :", solve_part1(cave))
+    cave = parse2(lines)
+    print("Solve part2 :", solve_part2(cave))
 
 
 if __name__ == "__main__":
