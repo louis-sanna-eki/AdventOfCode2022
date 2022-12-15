ebc65462b060174e02203607b25bdcf007ec8d9d
Louis Sanna <louis.sanna@ekimetrics.com>
2022-12-15 14:13:58 +0100

    solve part2

--- days/day15/solve.py
+++ days/day15/solve.py
@@ -1,6 +1,7 @@
 from typing import List, Tuple, Set
 
 Y_TO_CHECK = 2000000
+COORD_MAX = 4000000
 
 
 def manhattan_distance(start: Tuple[int, int], end: Tuple[int, int]) -> int:
@@ -64,6 +65,33 @@ def solve_part1(pairs: List[Tuple[Tuple[int, int], Tuple[int, int]]]):
     return len(safes - beacons)
 
 
+def solve_part2(pairs: List[Tuple[Tuple[int, int], Tuple[int, int]]]):
+    beacons: Set[Tuple[int, int]] = set()
+    for pair in pairs:
+        _, beacon = pair
+        beacons.add(beacon)
+    candidate = (0, 0)
+    while True:
+        if candidate[1] >= COORD_MAX + 1:
+            candidate = (candidate[0] + 1, 0)
+            continue
+        if candidate[0] >= COORD_MAX + 1:
+            raise Exception("Every line have been tested without finding a candidate")
+        is_candidate_valid = True
+        for sensor, beacon in pairs:
+            distance_to_boundary = manhattan_distance(
+                beacon, sensor
+            ) - manhattan_distance(candidate, sensor)
+            if distance_to_boundary < 0:
+                continue
+            is_candidate_valid = False
+            candidate = (candidate[0], candidate[1] + distance_to_boundary + 1)
+        if is_candidate_valid:
+            break
+    x, y = candidate
+    return x * 4000000 + y
+
+
 with open("input.txt", encoding="utf-8") as file_descriptor:
     lines = file_descriptor.read().strip().split("\n")
 
@@ -74,6 +102,8 @@ def main():
     """
     pairs = parse(lines)
     print("Solve part1 :", solve_part1(pairs))
+    pairs = parse(lines)
+    print("Solve part2 :", solve_part2(pairs))
 
 
 if __name__ == "__main__":
