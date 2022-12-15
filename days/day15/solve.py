1e176a782f2df70abdaad676795ba004cc7e9531
Louis Sanna <louis.sanna@ekimetrics.com>
2022-12-15 12:18:26 +0100

    solve part1

--- days/day15/solve.py
+++ days/day15/solve.py
@@ -1,7 +1,15 @@
-from typing import List
+from typing import List, Tuple, Set
+
+Y_TO_CHECK = 2000000
 
 
-def parse(lines: List[str]):
+def manhattan_distance(start: Tuple[int, int], end: Tuple[int, int]) -> int:
+    x1, y1 = start
+    x2, y2 = end
+    return abs(x1 - x2) + abs(y1 - y2)
+
+
+def parse(lines: List[str]) -> List[Tuple[Tuple[int, int], Tuple[int, int]]]:
     result = []
     for line in lines:
         sensor = tuple(
@@ -30,8 +38,30 @@ def parse(lines: List[str]):
     return result
 
 
-def solve_part1():
-    return 0
+def solve_part1(pairs: List[Tuple[Tuple[int, int], Tuple[int, int]]]):
+    safes: Set[Tuple[int, int]] = set()
+    beacons: Set[Tuple[int, int]] = set()
+    for pair in pairs:
+        sensor, beacon = pair
+        beacons.add(beacon)
+        radius = manhattan_distance(sensor, beacon)
+        sensor_projection = (sensor[0], Y_TO_CHECK)
+        if radius < manhattan_distance(sensor_projection, sensor):
+            continue
+        safes.add(sensor_projection)
+        safe = sensor_projection
+        while True:
+            safe = (safe[0] + 1, safe[1])
+            if radius < manhattan_distance(safe, sensor):
+                break
+            safes.add(safe)
+        safe = sensor_projection
+        while True:
+            safe = (safe[0] - 1, safe[1])
+            if radius < manhattan_distance(safe, sensor):
+                break
+            safes.add(safe)
+    return len(safes - beacons)
 
 
 with open("input.txt", encoding="utf-8") as file_descriptor:
@@ -42,8 +72,8 @@ def main():
     """
     The main entry point of the program.
     """
-    parse(lines)
-    print("Solve part1 :", solve_part1())
+    pairs = parse(lines)
+    print("Solve part1 :", solve_part1(pairs))
 
 
 if __name__ == "__main__":
